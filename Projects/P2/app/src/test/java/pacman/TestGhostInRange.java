625bb07364493e39ec5177f031d28f30bf2ed77d
Gayathri <gayuk@terpmail.umd.edu>
2023-03-01 14:18:01 -0500

    test case adding
    wq
    
    end

--- Projects/P2/app/src/test/java/pacman/TestGhostInRange.java
+++ Projects/P2/app/src/test/java/pacman/TestGhostInRange.java
@@ -6,6 +6,12 @@ import java.awt.Color;
 public class TestGhostInRange extends TestCase {
 
   public void testGhostInRange() throws FileNotFoundException {
-    return;
+    NoFrame frame= newNoFrame();
+	PacMan pacman= frame.addPacMan(newLocation(1,2));
+	assertFalse(pacman.is_ghost_in_range());
+	Ghost ghostclyde= frame.addGhost(newLocation(1,3), "Clyde", Color.orange);
+	assertTrue(pacman.is_ghost_in_range());
+	Ghost ghostblinky= frame.addGhost(newLocation(4, 5), "Blinky", Color.red);
+	assertFalse(pacman.is_ghost_in_range());
   }
 }
