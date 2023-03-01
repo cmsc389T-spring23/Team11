6a9a0ccb9b56e7006d0866e2c6984a4614728871
ashrdev <ashrdev@gmail.com>
2023-03-01 18:35:56 -0500

    Update TestPacManMove.java

--- Projects/P2/app/src/test/java/pacman/TestPacManMove.java
+++ Projects/P2/app/src/test/java/pacman/TestPacManMove.java
@@ -8,6 +8,6 @@ public class TestPacManMove extends TestCase {
   public void testPacManMove() throws FileNotFoundException {
     NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
     PacMan pacman = frame.addPacMan(new Location(3, 4)); //Creates PacMan at location x, y
-    assertTrue(pacman.move());
+    assertTrue(pacman.move()); //Check if assertion is true
   }
 }
