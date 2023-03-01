4d084c899010facd6224201da7ba76201a1726c3
ashrdev <ashrdev@gmail.com>
2023-03-01 18:58:56 -0500

    readme

--- Projects/P2/app/src/test/java/pacman/TestGhostMove.java
+++ Projects/P2/app/src/test/java/pacman/TestGhostMove.java
@@ -7,6 +7,8 @@ public class TestGhostMove extends TestCase {
 
   public void testGhostMove() throws FileNotFoundException {
     Mainframe frame = new MainFrame(); //Creates A New Map With Walls and Tokens Initialized
+    x = 3;
+    y = 4;
     Ghost ghost = frame.addGhost(new Location(x, y), "Ghost1", Color.red); //Creates a red ghost named "name" at location x,y
     assertTrue(ghost.move());
   }
